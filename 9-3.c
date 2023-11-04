4555bf1b8c1604cf8b1ca518f565402041ba57c5
youn1575 <youn461515@gmail.com>
2023-11-05 01:31:04 +0900

    Create 9-3.c

---
+++ 9-3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N_STUDENT 5
+
+int main(int argc, char *argv[]) {
+	int i;
+	int grade[N_STUDENT];
+	int score[N_STUDENT];
+	
+	for(i=0;i<N_STUDENT;i++)
+		grade[i]=rand()%100+1;
+
+	//grade -> score copy
+	for(i=0;i<N_STUDENT;i++)
+		score[i]=grade[i];
+		
+	for(i=0;i<N_STUDENT;i++)
+		printf("score[%d]=%d (grade:%i)\n", i, score[i], grade[i]);		
+	
+	
+	return 0;
+}
