2c577e3b39459c3cadac78c023f1c712cfe89085
youn1575 <youn461515@gmail.com>
2023-11-05 02:24:31 +0900

    9-5

---
+++ 9-5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ROWS 3
+#define COLS 3
+
+void addMatrix(int A[][COLS], int B[][COLS], int C[][COLS])
+{
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            C[i][j] = A[i][j] + B[i][j];
+        }
+    }
+}
+
+void printMatrix(int A[][COLS])
+{
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            printf("%d", A[i][j]);
+            if (j < COLS - 1) {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main(int argc, char* argv[]) {
+    int A[ROWS][COLS] = {
+       {2, 3, 0},
+       {8, 9, 1},
+       {7, 0, 5} };
+
+    int B[ROWS][COLS] = {
+       {1, 0, 0},
+       {0, 1, 0},
+       {0, 0, 1} };
+
+    int C[ROWS][COLS];
+
+    addMatrix(A, B, C);
+    printMatrix(C);
+
+    return 0;
+}
