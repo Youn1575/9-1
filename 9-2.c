11871e0dac1882f2210440b3f3ee034a0a59c599
youn1575 <youn461515@gmail.com>
2023-11-04 17:13:09 +0900

    Create 9-2.c

---
+++ 9-2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N_STUDENT 5
+int main(int argc, char *argv[]) 
+{
+	int i;
+	int grade[N_STUDENT];
+	printf("input 5 scores: ");
+	
+	for (i=0; i<N_STUDENT;i++)
+		scanf("%d", &grade[i]);
+		
+	for (i=0; i<N_STUDENT;i++)
+		printf("grade[%d]=%d\n", i, grade[i]);
+	
+	system("PAUSE");
+	return 0;
+}
